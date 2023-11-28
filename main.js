58fc1fb892645251e3368bae55befdff7df5ed96
Raphael Sackey <raphaelsackey@Raphaels-Air.lan>
2023-11-27 21:05:28 -0500

    added last two pages

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector('.projects_button').addEventListener('click', function() {
+    window.location.href = 'portfolio.html';
+});
+
+
+document.querySelector('.linkdln_button').addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+});
+
+
+
+// i have not used Js in like 2 years and i have forgotten the syntext so dont mind my not being 
+// uneficient with the code and basically being specific and repeating myself lol.
