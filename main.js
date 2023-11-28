670d5141c0e7c33b5edfa364b3508ecdbff4a88f
Raphael Sackey <raphaelsackey@Raphaels-Air.lan>
2023-11-28 01:31:26 -0500

    added some interraction features

--- main.js
+++ main.js
@@ -1,11 +1,58 @@
-document.querySelector('.projects_button').addEventListener('click', function() {
-    window.location.href = 'portfolio.html';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var url = window.location.pathname;
+    var filename = url.substring(url.lastIndexOf('/')+1);
+
+    if (filename === 'index.html') {
+        document.querySelector('.projects_button').addEventListener('click', function() {
+            window.location.href = 'portfolio.html';
+        });
+        
+        
+        document.querySelector('.linkdln_button').addEventListener('click', function() {
+            window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+        });
+    
+        document.querySelector('#pb_1').addEventListener('click', function() {
+            window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+        })
+        document.querySelector('#pb_2').addEventListener('click', function() {
+            window.location.href = 'https://github.com/RaphaelSackey/Space-Station-and-Weather-tracker-application';
+        })
+        
+        document.querySelector('#pb_3').addEventListener('click', function() {
+            window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+        })
+        
+    }
+
+
+    if (filename === 'portfolio.html') {
+
+        document.querySelector('#pb_1').addEventListener('click', function() {
+            window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+        })
+        document.querySelector('#pb_2').addEventListener('click', function() {
+            window.location.href = 'https://github.com/RaphaelSackey/Space-Station-and-Weather-tracker-application';
+        })
+        
+        document.querySelector('#pb_3').addEventListener('click', function() {
+            window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
+        })
+    
+        document.querySelector('#pb_4').addEventListener('click', function() {
+            window.location.href = 'https://github.com/RaphaelSackey/Farm-manager-appliction-';
+        })
+       
+    }
+
 });
 
 
-document.querySelector('.linkdln_button').addEventListener('click', function() {
-    window.location.href = 'https://www.linkedin.com/in/raphael-sackey-1120381b1/';
-});
+
+
+
+
 
 
 
